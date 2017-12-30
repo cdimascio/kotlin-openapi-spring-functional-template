afaa7d5e03b4279e6817e91cfd56e914818be94f
Carmine DiMascio <dimascio@us.ibm.com>
2017-12-30 15:58:46 -0500

    refactor

--- src/main/kotlin/functional/Application.kt
+++ src/main/kotlin/functional/Application.kt
@@ -7,31 +7,31 @@ import org.springframework.web.server.adapter.WebHttpHandlerBuilder
 import reactor.ipc.netty.http.server.HttpServer
 import reactor.ipc.netty.tcp.BlockingNettyContext
 
-class Application {
+class Application(port: Int = dotenv["PORT"]?.toInt() ?: 8080) {
 	
 	private val httpHandler: HttpHandler
 	private val server: HttpServer
 	private var nettyContext: BlockingNettyContext? = null
-
-	constructor(port: Int = dotenv["PORT"]?.toInt() ?: 8080) {
-		val context = GenericApplicationContext().apply {
-			beans().initialize(this)
-			refresh()
-		}
-		server = HttpServer.create(port)
-		httpHandler = WebHttpHandlerBuilder.applicationContext(context).build()
-	}
 
 	fun start() {
 		nettyContext = server.start(ReactorHttpHandlerAdapter(httpHandler))
 	}
 	
 	fun startAndAwait() {
-		server.startAndAwait(ReactorHttpHandlerAdapter(httpHandler), { nettyContext = it })
+		server.startAndAwait(ReactorHttpHandlerAdapter(httpHandler)) { nettyContext = it }
 	}
 	
 	fun stop() {
 		nettyContext?.shutdown()
+	}
+
+	init {
+		val context = GenericApplicationContext().apply {
+			beans().initialize(this)
+			refresh()
+		}
+		server = HttpServer.create(port)
+		httpHandler = WebHttpHandlerBuilder.applicationContext(context).build()
 	}
 }
 
