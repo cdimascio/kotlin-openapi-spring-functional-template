f1ecb8c976ee6aba5055e726e03d94b0609b6886
Carmine DiMascio <dimascio@us.ibm.com>
2018-01-06 14:12:27 -0500

    update dotenv to latest

--- src/main/kotlin/functional/Env.kt
+++ src/main/kotlin/functional/Env.kt
@@ -1,11 +1,11 @@
 package functional
 
 import functional.models.Error
-import io.github.cdimascio.Dotenv
+import io.github.cdimascio.dotenv.dotenv
 import io.github.cdimascio.swagger.Validate
 
 val validate = Validate.configure("static/api.json") { status, messages ->
     Error(status.value(), messages)
 }
 
-val dotenv = Dotenv.configure().build()
+val dotenv = dotenv()
