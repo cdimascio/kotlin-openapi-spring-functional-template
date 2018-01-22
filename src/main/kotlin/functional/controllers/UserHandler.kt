8fb31c85a36f39393f52ea1e8013189fd878f776
Carmine DiMascio <dimascio@us.ibm.com>
2018-01-21 20:30:04 -0500

    create in-memory user db as mutable list

--- src/main/kotlin/functional/controllers/UserHandler.kt
+++ src/main/kotlin/functional/controllers/UserHandler.kt
@@ -7,22 +7,27 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
+import reactor.core.publisher.*
 
 class UserHandler {
 
-    private val users = Flux.just(
-            User("carmine", "d"),
-            User("eliana", "g"),
-            User("laura", "h"))
-
+    private var users = mutableListOf(
+        User("carmine", "d"),
+        User("eliana", "g"),
+        User("laura", "h")
+    )
     fun findAll(req: ServerRequest): Mono<ServerResponse> = validate
-            .request(req) { ok().body(users) }
+            .request(req) {
+                val result = Flux.fromIterable(users)
+                ok().body(result)
+            }
             .onErrorResume { t -> internalServerError(t) }
 
     fun create(req: ServerRequest) = validate
             .request(req)
-            .withBody(User::class.java) { body -> ok().body(Mono.just(body)) }
+            .withBody(User::class.java) { user ->
+                users.add(user)
+                ok().body(Mono.just(user))
+            }
             .onErrorResume { t-> internalServerError(t) }
 }
