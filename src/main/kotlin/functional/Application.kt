83279a1b39fc74f99bc680ff54d485ea16a5c730
Carmine DiMascio <dimascio@us.ibm.com>
2018-11-08 10:49:18 -0500

    trying to update spring

--- src/main/kotlin/functional/Application.kt
+++ src/main/kotlin/functional/Application.kt
@@ -6,25 +6,42 @@ import org.springframework.context.support.GenericApplicationContext
 import org.springframework.http.server.reactive.HttpHandler
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder
-import reactor.ipc.netty.http.server.HttpServer
-import reactor.ipc.netty.tcp.BlockingNettyContext
+import reactor.netty.DisposableServer
+import reactor.netty.http.server.HttpServer
+// import reactor.ipc.netty.http.server.HttpServer
+// import reactor.ipc.netty.tcp.BlockingNettyContext
+//import reactor.netty.http.server.HttpServer
+import java.time.Duration
 
 class Application(port: Int = dotenv["PORT"]?.toInt() ?: 8080) {
 
     private val httpHandler: HttpHandler
     private val server: HttpServer
-    private var nettyContext: BlockingNettyContext? = null
+    //    private var nettyContext: BlockingNettyContext? = null
+    private var disposableServer: DisposableServer? = null
 
     fun start() {
-        nettyContext = server.start(ReactorHttpHandlerAdapter(httpHandler))
+//        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(handler);
+//        HttpServer.create("0.0.0.0", this.port).newHandler(handler).block()
+//        nettyContext = server.start(ReactorHttpHandlerAdapter(httpHandler))
+//        val adapter = ReactorHttpHandlerAdapter(handler)
+//        server.newHandler(adapter)
+
+
+        server.bindUntilJavaShutdown(Duration.ofSeconds(5)) {
+            disposableServer = it
+        }
     }
 
     fun startAndAwait() {
-        server.startAndAwait(ReactorHttpHandlerAdapter(httpHandler)) { nettyContext = it }
+//        serverstartAndAwait(ReactorHttpHandlerAdapter(httpHandler)) { nettyContext = it }
+//        server.
+        start()
     }
 
     fun stop() {
-        nettyContext?.shutdown()
+//        nettyContext?.shutdown()
+        disposableServer?.disposeNow()
     }
 
     init {
@@ -32,8 +49,16 @@ class Application(port: Int = dotenv["PORT"]?.toInt() ?: 8080) {
             beans().initialize(this)
             refresh()
         }
-        server = HttpServer.create(port)
         httpHandler = WebHttpHandlerBuilder.applicationContext(context).build()
+        val adapter = ReactorHttpHandlerAdapter(httpHandler)
+        server = HttpServer.create().handle(adapter).port(port) // port)
+//        server.port(port)
+
+//        server.handle(adapter)
+//        println(server)
+//        server.
+
+//        httpHandler = WebHttpHandlerBuilder.applicationContext(context).build()
     }
 }
 
